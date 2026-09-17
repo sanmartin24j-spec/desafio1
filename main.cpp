ebefe9cbeffc3846ed0cf856bef04a7482b39f8d
Jonathansanmart <jonathan.sanmartin@udea.edu.co>
2026-09-17 00:45:34 -0500

    Lectura y escritura de fichas entre dos bytes

--- main.cpp
+++ main.cpp
@@ -1,42 +1,73 @@
 #include <iostream>
-
 using namespace std;
+void escribirFicha(unsigned char *tablero, int byteActual, int desplazamiento, unsigned char ficha);
+unsigned char leerFicha(unsigned char *tablero, int byteActual, int desplazamiento);
 
-
-unsigned char leerficha(unsigned char byte, int desplazamiento);
-
-unsigned char escribirficha(unsigned char byte, unsigned char ficha, int desplazamiento);
 
 int main()
 {
-    unsigned char byte = 0b10010100;
-    unsigned char lectura= leerficha(byte, 3);
-    unsigned char ficha = 0b111;
-    byte = escribirficha(byte, ficha, 3);
-    unsigned char lecturaNueva = leerficha(byte, 3);
+    unsigned char tablero[2];
 
-    cout <<"anterior "<< (int) lectura<<endl;
-    cout <<"nueva "<< (int) lecturaNueva;
+    tablero[0] = 0b11101010;
+    tablero[1] = 0b10110010;
+    unsigned char ficha = 0b110;
+    int desplazamiento = 7;
+    escribirFicha(tablero, 0, desplazamiento, ficha);
+
+
+    unsigned char lectura =leerFicha(tablero, 0, desplazamiento);
+    cout << "Ficha leida: "<< (int)lectura<< endl;
 
     return 0;
 }
 
-
-unsigned char leerficha(unsigned char byte, int desplazamiento)
+void escribirFicha(unsigned char *tablero, int byteActual, int desplazamiento, unsigned char ficha)
 {
-    byte = byte >> desplazamiento;
-    byte = byte & 0b00000111;
 
-    return byte;
+    ficha= ficha&0b00000111;
+
+    if (desplazamiento <= 5){
+        unsigned char zona = 0b0000111<<desplazamiento;
+
+        unsigned char limpieza = ~zona;
+
+        tablero[byteActual] =  tablero[byteActual]&limpieza;
+
+        tablero[byteActual] =  tablero[byteActual] | (ficha << desplazamiento); }
+
+    else{
+        int bitsprimerbyte= 8 - desplazamiento;
+        int bitssegundobyte= 3 - bitsprimerbyte;
+        unsigned char mascaraprimerbyte = (1 << bitsprimerbyte) - 1;
+        unsigned char zonaprimerbyte = mascaraprimerbyte << desplazamiento;
+        unsigned char limpiezaprimerbyte =~zonaprimerbyte;
+        tablero[byteActual] =tablero[byteActual] & limpiezaprimerbyte;
+
+        tablero[byteActual] =tablero[byteActual] |((ficha & mascaraprimerbyte) << desplazamiento);
+
+
+        unsigned char mascarasegundobyte =(1 << bitssegundobyte) - 1;
+        unsigned char limpiezasegundobyte =~mascarasegundobyte;
+        tablero[byteActual + 1] = tablero[byteActual + 1] & limpiezasegundobyte;
+
+        tablero[byteActual + 1] = tablero[byteActual + 1] |((ficha >> bitsprimerbyte) & mascarasegundobyte);}
+
 }
 
-unsigned char escribirficha(unsigned char byte, unsigned char ficha,int desplazamiento)
+
+unsigned char leerFicha(unsigned char *tablero, int byteActual, int desplazamiento)
 {
-    ficha = ficha & 0b00000111;
-    unsigned char zona = 0b00000111 << desplazamiento;
-    unsigned char limpieza = ~zona;
-    byte = byte & limpieza;
-    unsigned char fichamovida = ficha << desplazamiento;
-    byte = byte | fichamovida;
-    return byte;
+    if (desplazamiento <= 5){
+        return (tablero[byteActual] >> desplazamiento)& 0b00000111;}
+    else {
+        int bitsprimerbyte = 8 - desplazamiento;
+        int bitssegundobyte = 3 - bitsprimerbyte;
+        unsigned char mascaraprimerbyte =(1 << bitsprimerbyte) - 1;
+        unsigned char mascarasegundobyte =(1 << bitssegundobyte) - 1;
+        unsigned char parteprimerbyte =(tablero[byteActual] >> desplazamiento) & mascaraprimerbyte;
+        unsigned char partesegundobyte =tablero[byteActual + 1] & mascarasegundobyte;
+
+        return parteprimerbyte |(partesegundobyte << bitsprimerbyte); }
+
+
 }
